f415d927ab874dd1716fc58358f75cb35535ac92
meng <mengtee1127@gmail.com>
2025-12-28 03:57:56 +0800

    railway start command

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,6 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
