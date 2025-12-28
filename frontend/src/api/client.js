d43a2764042a34b59099921521c072fd117c5e76
meng <mengtee1127@gmail.com>
2025-12-28 12:33:58 +0800

    chores: cloudflare

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log('VUE_APP_API_URL:', process.env.VUE_APP_API_URL);
 
 const apiClient = axios.create({
-    baseURL: 'https://price-aaron-onto-bought.trycloudflare.com',
+    baseURL: 'https://charge-meat-restored-detected.trycloudflare.com',
     headers: {
         'Content-Type': 'application/json'
     }
